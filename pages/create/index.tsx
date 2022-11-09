74d29d9d17fd2954512c1e537f9b523052b3f9b2
Henrik Elgemark <henrik.elgemark@funkismultimedia.se>
2022-11-09 11:13:28 +0100

    Updates

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -1,8 +1,7 @@
-import Layout from "../../components/Layout"
 import Head from "next/head"
 
 const Create = () => {
-    return <><Head><title >create</title></Head><Layout><h3>Create</h3></Layout></>
+    return <><Head><title >create</title></Head><h3>Create</h3></>
 }
 
 export default Create
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,31 @@
 
-import { getPostsByString,getPostsByTags, useInit } from "../utils/firebase"
 import Layout from '../components/Layout'
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '../config/theme';
 import Head from "next/head";
-import { useState,useEffect } from "react";
-import { useRouter } from "next/router";
-import {useDebouncedQuery} from "../utils/queryUtils"
+import { useDebouncedQuery } from "../utils/queryUtils"
+import { getPostsByTags } from '../utils/firebase/api';
 
 export default function IndexPage({posts = []}) {
   
-  const { isInitialized: isFirebaseInitialized } = useInit()
-  const [searchStr, setSearchStr] = useState("");
   const {set:setQuery} =  useDebouncedQuery(1000)
-
+  
 
   return (<>
     <Head>
       <meta name="viewport" content="initial-scale=1, width=device-width" />
     </Head>
     <ThemeProvider theme={theme}><CssBaseline />
-      {isFirebaseInitialized && <Layout onSearch={(value) => { setQuery({search:value}); setSearchStr(value) }}>
+      <Layout onSearch={(value) => { setQuery({search:value}) }}>
         {posts.map((data, index) => <h3 key={index}>{data.title}</h3>)}
-        </Layout>}
+        </Layout>
     </ThemeProvider>
   </>);
 }
 
-export async function getServerSideProps({query}) {
-  const posts = await getPostsByTags(query.search.split(" "));
+export async function getServerSideProps({ query }) {
+  const tags = query.search?.split(" ");
+  const posts = tags ? await getPostsByTags(tags) : [];
   return {props:{posts}}
 }
