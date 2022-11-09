831b2b50ef56e383e90bdaf8b8a8d30559f12a02
Henrik Elgemark <henrik.elgemark@funkismultimedia.se>
2022-11-09 11:54:49 +0100

    Update editable splash

--- components/splashes/SplashEditable.tsx
+++ components/splashes/SplashEditable.tsx
@@ -5,12 +5,10 @@ import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SaveIcon from "@mui/icons-material/Save";
 import TextField from "@mui/material/TextField";
+
 
 const SplashEditable = ({  title = "Title", body = "Body", media = {} }) => {
   return (
@@ -21,11 +19,6 @@ const SplashEditable = ({  title = "Title", body = "Body", media = {} }) => {
             R
           </Avatar>
         }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
         title={<TextField fullWidth label="Title" placeholder="Title"/>}
         // subheader="September 14, 2016"
       />
@@ -34,11 +27,8 @@ const SplashEditable = ({  title = "Title", body = "Body", media = {} }) => {
         <TextField fullWidth multiline label="body" placeholder="body"/>              
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-            <FavoriteIcon />
-        </IconButton>
         <IconButton aria-label="share">
-          <ShareIcon />
+          <SaveIcon />
         </IconButton>
       </CardActions>
     </Card>
