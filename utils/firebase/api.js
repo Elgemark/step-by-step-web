bcb00deffad347232b4008457c5f3b0d6827ecb2
Henrik Elgemark <henrik.elgemark@funkismultimedia.se>
2022-11-09 11:00:55 +0100

    Updates

--- utils/firebase/api.js
+++ utils/firebase/api.js
@@ -1,12 +1,14 @@
-import { database, collection, query } from "@lib/firebase";
+import { getFirestore, collection, where, query, getDocs } from "firebase/firestore";
 
 export const getPostsByTags = async (tags = []) => {
-  const stepsRef = collection(database, "posts");
-
+  const firebase = getFirestore();
+  const stepsRef = collection(firebase, "posts");
   const queryBuild = query(stepsRef, where("tags", "array-contains-any", tags));
+
   let data = [];
   try {
     const querySnapshot = await getDocs(queryBuild);
+    console.count("query");
     querySnapshot.forEach((doc) => {
       data.push(doc.data());
     });
