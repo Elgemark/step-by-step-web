d8aab9a4161f09cafae59bd95eb49bb5bb5f7882
Henrik Elgemark <henrik.elgemark@funkismultimedia.se>
2022-12-13 11:59:27 +0100

    Refactoring

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -3,15 +3,9 @@ import Typography from "@mui/material/Typography";
 import UserAvatar from "./UserAvatar";
 import { useUser } from "../utils/firebase/api";
 import { FC } from "react";
-import { Button, Stack, useTheme } from "@mui/material";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import { SyntheticEvent, MouseEventHandler } from "react";
-import BookmarkIcon from "@mui/icons-material/Bookmark";
-import CreateIcon from "@mui/icons-material/Create";
-import AssistantDirectionIcon from "@mui/icons-material/AssistantDirection";
+import { Stack, useTheme } from "@mui/material";
+import { MouseEventHandler } from "react";
 import styled from "styled-components";
-import LoadingButton from "@mui/lab/LoadingButton";
 
 const Root = styled.div`
   padding: ${({ theme }) => theme.spacing(2)};
@@ -28,21 +22,13 @@ const Root = styled.div`
   }
 `;
 
-const tabProps = (index: number) => {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-};
 const UserCard: FC<{
-  onTabChange: (event: SyntheticEvent<Element, Event>, value: any) => void;
   onFollow: MouseEventHandler<HTMLButtonElement>;
   onUnfollow: MouseEventHandler<HTMLButtonElement>;
-  tabValue: string;
   userId?: string;
   isFollowing: boolean;
   loadingFollower: boolean;
-}> = ({ tabValue, onTabChange, onFollow, onUnfollow, userId, isFollowing, loadingFollower = false, ...props }) => {
+}> = ({ onFollow, onUnfollow, userId, isFollowing, loadingFollower = false, ...props }) => {
   const theme = useTheme();
   const { data: user, update, isLoading } = useUser(userId);
 
@@ -56,22 +42,8 @@ const UserCard: FC<{
         <Typography className="user-description" variant="body2" color="text.secondary">
           {user?.description || ""}
         </Typography>
-        {isFollowing ? (
-          <LoadingButton className="button-follow" variant="contained" onClick={onUnfollow} loading={loadingFollower}>
-            Unfollow
-          </LoadingButton>
-        ) : (
-          <LoadingButton className="button-follow" variant="contained" onClick={onFollow} loading={loadingFollower}>
-            Follow
-          </LoadingButton>
-        )}
       </Stack>
       <Divider />
-      <Tabs value={tabValue} onChange={onTabChange} aria-label="post tabs">
-        <Tab label="Created" icon={<CreateIcon />} {...tabProps(1)} value="created" />
-        <Tab label="Saved" icon={<BookmarkIcon />} {...tabProps(0)} value="saved" />
-        <Tab label="Follows" icon={<AssistantDirectionIcon />} {...tabProps(2)} value="follows" />
-      </Tabs>
     </Root>
   );
 };
--- pages/user/[...user].tsx
+++ pages/user/[...user].tsx
@@ -5,9 +5,36 @@ import { useRouter } from "next/router";
 import Posts from "../../components/posts/Posts";
 import UserCard from "../../components/UserCard";
 import { SyntheticEvent } from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import CreateIcon from "@mui/icons-material/Create";
+import AssistantDirectionIcon from "@mui/icons-material/AssistantDirection";
+import LoadingButton from "@mui/lab/LoadingButton";
+import styled from "styled-components";
+import { useTheme } from "@mui/material";
+
+const StyledLayout = styled(Layout)`
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .tabs {
+    margin: ${({ theme }) => theme.spacing(4)};
+  }
+`;
+
+const tabProps = (index: number) => {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+};
 
 const Index = ({ posts, uid, tabValue }) => {
   const router = useRouter();
+  const theme = useTheme();
   const { isFollowing, toggle, isLoading } = useFollow(uid);
 
   const onTabChangehandle = (event: SyntheticEvent, newValue: string) => {
@@ -23,18 +50,30 @@ const Index = ({ posts, uid, tabValue }) => {
       <Head>
         <title>STEPS | User</title>
       </Head>
-      <Layout>
+      <StyledLayout theme={theme}>
         <UserCard
-          onTabChange={onTabChangehandle}
           userId={uid}
-          tabValue={tabValue}
           onFollow={onFollowHandler}
           onUnfollow={onFollowHandler}
           isFollowing={isFollowing}
           loadingFollower={isLoading}
         />
+        {isFollowing ? (
+          <LoadingButton className="button-follow" variant="contained" onClick={onFollowHandler} loading={isLoading}>
+            Unfollow
+          </LoadingButton>
+        ) : (
+          <LoadingButton className="button-follow" variant="contained" onClick={onFollowHandler} loading={isLoading}>
+            Follow
+          </LoadingButton>
+        )}
+        <Tabs className="tabs" value={tabValue} onChange={onTabChangehandle} aria-label="post tabs">
+          <Tab label="Created" icon={<CreateIcon />} {...tabProps(1)} value="created" />
+          <Tab label="Saved" icon={<BookmarkIcon />} {...tabProps(0)} value="saved" />
+          <Tab label="Follows" icon={<AssistantDirectionIcon />} {...tabProps(2)} value="follows" />
+        </Tabs>
         <Posts posts={posts} enableLink />
-      </Layout>
+      </StyledLayout>
     </>
   );
 };
