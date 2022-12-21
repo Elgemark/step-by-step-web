4b83808c9c5ff585eccc7799a00d32c83aa1ee76
Henrik Elgemark <henrik.elgemark@funkismultimedia.se>
2022-12-21 09:55:59 +0100

    Update useStateObject

--- utils/object.ts
+++ utils/object.ts
@@ -6,14 +6,16 @@ export const useStateObject = (obj = {}) => {
 
   return {
     object,
-    setValue: (path, value) => {
+    setValue: (path: Array<string | number> | string, value: any): object => {
       const updatedObject = _.cloneDeep(object);
       _.set(updatedObject, path, value);
       setObject(updatedObject);
+      return updatedObject;
     },
-    replace: (object) => {
+    replace: (object: object): object => {
       setObject(object);
+      return object;
     },
-    getValue: (path, defaultValue) => _.get(object, path) || defaultValue,
+    getValue: (path: Array<string | number> | string, defaultValue?: any): any => _.get(object, path) || defaultValue,
   };
 };
