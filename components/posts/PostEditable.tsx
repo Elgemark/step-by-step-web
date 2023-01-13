9fa8c922fa387a699620bc64597575b0577c46e5
Henrik Elgemark <henrik.elgemark@funkismultimedia.se>
2023-01-13 15:51:23 +0100

    Code clean

--- components/posts/PostEditable.tsx
+++ components/posts/PostEditable.tsx
@@ -1,8 +1,6 @@
 import { Button, Card, CardActions, Chip, Collapse } from "@mui/material";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
-import Avatar from "@mui/material/Avatar";
-import { red } from "@mui/material/colors";
 import TextField from "@mui/material/TextField";
 import Fab from "@mui/material/Fab";
 import Stack from "@mui/material/Stack";
