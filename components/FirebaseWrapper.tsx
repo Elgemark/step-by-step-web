41d3d9244b3ef52d5d600a8876a1e604c9edf792
Henrik Elgemark <henrik.elgemark@funkismultimedia.se>
2023-01-26 09:01:51 +0100

    Update firebase init functionality

--- components/FirebaseWrapper.tsx
+++ components/FirebaseWrapper.tsx
@@ -1,5 +1,6 @@
-import { getAuth, connectAuthEmulator } from "firebase/auth"; // Firebase v9+
-import { getDatabase, connectDatabaseEmulator } from "firebase/database"; // Firebase v9+
+import { getApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; // Firebase v9+
+import { getDatabase } from "firebase/database"; // Firebase v9+
 
 import { FirebaseAppProvider, DatabaseProvider, AuthProvider, useFirebaseApp } from "reactfire";
 
@@ -13,14 +14,14 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-const FirebaseCredentials = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-};
-
 const FirebaseAppProviderWrapper = ({ children }) => {
-  return <FirebaseAppProvider firebaseConfig={firebaseConfig}>{children}</FirebaseAppProvider>;
+  // Check if app already initialized...
+  const app = getApp();
+  return (
+    <FirebaseAppProvider firebaseApp={app || undefined} firebaseConfig={app ? undefined : firebaseConfig}>
+      {children}
+    </FirebaseAppProvider>
+  );
 };
 
 const AuthAndDatabaseProviderWrapper = ({ children }) => {
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,15 +1,16 @@
-import { useEffect, FC, useCallback } from "react";
+import { useEffect, useCallback } from "react";
 import Head from "next/head";
 import Layout from "../../components/Layout";
 import styled from "styled-components";
 import { CircularProgress } from "@mui/material";
 // Firebase related
-import { useAuthState } from "react-firebase-hooks/auth";
 import { getAuth } from "firebase/auth";
 import firebase from "firebase/compat/app";
 import { config as uiConfig } from "../../config/firebaseAuthUI";
 // CSS
 import "firebaseui/dist/firebaseui.css";
+import { useUser } from "reactfire";
+import FirebaseWrapper from "../../components/FirebaseWrapper";
 
 const StyledContainer = styled.div`
   height: calc(100vh - 50%);
@@ -19,25 +20,20 @@ const StyledContainer = styled.div`
   align-items: center;
 `;
 
-interface Props {
-  firebaseClient: typeof firebase;
-  config: firebaseui.auth.Config;
-}
-
-const LogIn: FC<Props> = ({ firebaseClient, config }) => {
-  const [loading] = useAuthState(getAuth());
+const LogIn = () => {
+  const { status } = useUser();
 
   const loadFirebaseui = useCallback(async () => {
     const firebaseui = await import("firebaseui");
     const firebaseUi = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(getAuth());
     firebaseUi.start(".firebaseui-auth-container", uiConfig(firebase));
-  }, [firebaseClient, config]);
+  }, []);
 
   useEffect(() => {
-    if (!loading) {
+    if (status !== "loading") {
       loadFirebaseui();
     }
-  }, [loading]);
+  }, [status]);
 
   return (
     <>
@@ -46,7 +42,7 @@ const LogIn: FC<Props> = ({ firebaseClient, config }) => {
       </Head>
       <Layout>
         <StyledContainer>
-          {loading && <CircularProgress />}
+          {status === "loading" ? <CircularProgress /> : null}
           <div className="firebaseui-auth-container" />
         </StyledContainer>
       </Layout>
@@ -54,4 +50,8 @@ const LogIn: FC<Props> = ({ firebaseClient, config }) => {
   );
 };
 
-export default LogIn;
+export default () => (
+  <FirebaseWrapper>
+    <LogIn></LogIn>
+  </FirebaseWrapper>
+);
