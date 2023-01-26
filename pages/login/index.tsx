d2b831bf7e872f2bdf30d16369642dbb02b61428
Henrik Elgemark <henrik.elgemark@funkismultimedia.se>
2023-01-26 15:11:20 +0100

    Fix login margin

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -14,7 +14,7 @@ import FirebaseWrapper from "../../components/wrappers/FirebaseWrapper";
 import MUIWrapper from "../../components/wrappers/MUIWrapper";
 
 const StyledContainer = styled.div`
-  height: calc(100vh - 50%);
+  margin-top: 100px;
   width: 100%;
   display: flex;
   justify-content: center;
