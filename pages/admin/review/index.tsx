ef683b4254b8e86b95ebf861e6cd5a2bcc286626
Henrik Elgemark <henrik.elgemark@funkismultimedia.se>
2023-01-31 14:02:26 +0100

    Update admin

--- pages/admin/review/index.tsx
+++ pages/admin/review/index.tsx
@@ -3,16 +3,50 @@ import { getPostsByQuery } from "../../../utils/firebase/api/post";
 import Collection from "../../../classes/Collection";
 import FirebaseWrapper from "../../../components/wrappers/FirebaseWrapper";
 import MUIWrapper from "../../../components/wrappers/MUIWrapper";
-import { useUser } from "../../../utils/firebase/api";
+import { likePost, useUser } from "../../../utils/firebase/api";
 import Loader from "../../../components/Loader";
 import { limit, startAfter, where } from "firebase/firestore";
 import PageMain from "../../../components/PageMain";
+import Head from "next/head";
+import Layout from "../../../components/Layout";
+import Masonry from "../../../components/primitives/Masonry";
+import DialogReport, { ReportData } from "../../../components/DialogReport";
+import DialogDeletePost from "../../../components/DialogDeletePost";
+import PostMoreMenu from "../../../components/PostMoreMenu";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import Post from "../../../components/posts/Post";
 
 const collection = new Collection();
 let lastDoc;
 
-const ReviewPage = (props) => {
+const ReviewPage = ({ posts }) => {
+  const router = useRouter();
+
+  const [report, setReport] = useState<ReportData>();
+  const [deletePost, setDeletePost] = useState<string>();
+
   const { isLoading, data: user } = useUser();
+
+  if (!posts.length) {
+    return null;
+  }
+
+  const onEditHandler = ({ id }) => {
+    router.push("/create/" + id);
+  };
+
+  const onLikeHandler = async ({ id }) => {
+    await likePost(id);
+  };
+
+  const onClickAvatarHandler = ({ userId }) => {
+    if (user.uid === userId) {
+      router.push("/profile/" + userId);
+    } else {
+      router.push("/user/" + userId);
+    }
+  };
 
   if (isLoading) {
     return <Loader />;
@@ -22,7 +56,52 @@ const ReviewPage = (props) => {
     return <h1>Access denied!</h1>;
   }
 
-  return <PageMain {...props} title="STEPS" enableLink={true} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <title>{"Admin"}</title>
+      </Head>
+      <Layout>
+        <Masonry>
+          {posts.map((data, index) => (
+            <Post
+              key={index}
+              currentUserId={user?.uid}
+              enableLink={true}
+              hrefBasePath="/admin/review/post/"
+              action={
+                <PostMoreMenu
+                  onEdit={
+                    user?.uid === data.userId
+                      ? () => {
+                          onEditHandler(data);
+                        }
+                      : undefined
+                  }
+                  onDelete={
+                    user?.uid === data.userId
+                      ? () => {
+                          setDeletePost(data.id);
+                        }
+                      : undefined
+                  }
+                  onReport={() => setReport({ postId: data.id, userId: user.uid })}
+                />
+              }
+              onLike={() => onLikeHandler(data)}
+              onClickAvatar={() => onClickAvatarHandler(data)}
+              {...data}
+            />
+          ))}
+        </Masonry>
+        {/* DELETE DIALOG */}
+        <DialogDeletePost open={deletePost} onClose={() => setDeletePost(null)} />
+        {/* REPORT DIALOG */}
+        <DialogReport open={report} onClose={() => setReport(null)} />
+      </Layout>
+    </>
+  );
 };
 
 export async function getServerSideProps({ query }) {
